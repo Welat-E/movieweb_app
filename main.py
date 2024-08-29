4ceb7cc5d92c90648b7ccdf1c705d7ec49c03f5e
Welat-E <welaterenug@gmail.com>
2024-08-29 13:04:17 +0200

    Update main.py
    Creating class Datamanger

---
+++ main.py
@@ -0,0 +1,48 @@
+#from Flask import flask
+
+class Datamanger():
+    def __init__(self):
+        self.users = {}
+        self.movies = {}
+
+    def add_user(self, user_id, name):
+        """Adds a new user"""
+        if user_id not in self.users:
+             self.users[user_id] = {'name': name}
+        else:
+             print("User exists already.")
+
+    
+    def add_movie(self, movie_id, name, director, year, rating):
+        """Adds a new movie."""
+        if movie_id not in self.movies:
+            self.movies[movie_id] = {'name': name, 'director': director, 'year': year, 'rating': rating}
+        else:
+            print("Movie already exists.")
+
+
+    def update_movie(self, movie_id, name=None, director=None, year=None, rating=None):
+        """Updates details of a movie."""
+        if movie_id in self.movies:
+            if name is not None:
+                self.movies[movie_id]['name'] = name
+            if director is not None:
+                self.movies[movie_id]['director'] = director
+            if year is not None:
+                self.movies[movie_id]['year'] = year
+            if rating is not None:
+                self.movies[movie_id]['rating'] = rating
+
+        else:
+            print("Movies doesn't exist.")
+
+
+    def delete_movie(self, movie_id):
+        """Deltes movies from storage."""
+        if movie_id in self.movies:
+            del self.movies['movie_id']
+        else:
+            print("Movie not found.")
+
+
+    

fa51bb2791b11cbf2543b7cbed20041a9d364c5f
Welat-E <welaterenug@gmail.com>
2024-09-01 14:06:37 +0200

    Update app.py
    Doing some changes

--- app.py
+++ app.py
@@ -1,17 +1,14 @@
-#from crypt import methods
 from flask import Flask, request, render_template, url_for, redirect
 from datamanager.sqlite_data_manager import SQLiteDataManager, User, db, Movie
 
-
 app = Flask(__name__)
 
-# initialise SQLAlchemy with Flask-App
+# Initialisiere SQLAlchemy mit der Flask-App
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data/users_movies.sqlite'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db.init_app(app)
 
-
-# initialise the data_manager
+# Initialisiere den Datenbankmanager
 data_manager = SQLiteDataManager("data/users_movies.sqlite")
 
 @app.route('/')
@@ -21,12 +18,12 @@ def home():
 @app.route('/users', methods=['GET'])
 def list_users():
     users = data_manager.get_all_users()
-    return str(users)  #convert the list to a string for a better resp.
+    return render_template('users.html', users=users)  # Verwende das Template für die Benutzerliste
 
 @app.route('/users/<int:user_id>')
 def get_user_movies(user_id):
     movies = data_manager.get_user_movies(user_id)
-    return str(movies)
+    return render_template('user_movies.html', user_id=user_id, movies=movies)  # Verwende ein Template, um Filme anzuzeigen
 
 @app.route('/add_user', methods=['GET', 'POST'])
 def add_user():
@@ -36,19 +33,18 @@ def add_user():
         record = User(id=user_id, name=name)
         db.session.add(record)
         db.session.commit()
-        return redirect(url_for('home'))  # Redirect to home
+        return redirect(url_for('home'))
     return render_template('add_user.html')
 
-@app.route('/users/<user_id>/add_movie', methods=['GET', 'POST'])
-def add_movie():
+@app.route('/users/<int:user_id>/add_movie', methods=['GET', 'POST'])
+def add_movie(user_id):
     if request.method == 'POST':
         title = request.form['name']
-        record = Movie(name=title)
-        db.session.add(record)
+        movie = Movie(name=title, user_id=user_id)  # Füge den user_id hinzu
+        db.session.add(movie)
         db.session.commit()
-        return redirect('home')
-    return render_template('/users/<user_id>/add_movie')
-
+        return redirect(url_for('get_user_movies', user_id=user_id))
+    return render_template('add_movie.html', user_id=user_id)
 
 @app.route('/users/<int:user_id>/update_movie/<int:movie_id>', methods=['GET', 'POST'])
 def update_movie(user_id, movie_id):
@@ -56,26 +52,18 @@ def update_movie(user_id, movie_id):
 
     if request.method == 'POST':
         new_title = request.form['name']
-        movie.name = new_title  # add the new title
-        db.session.commit()  #save
+        movie.name = new_title
+        db.session.commit()
         return redirect(url_for('get_user_movies', user_id=user_id))
 
     return render_template('update_movie.html', user_id=user_id, movie=movie)
 
 @app.route('/users/<int:user_id>/delete_movie/<int:movie_id>', methods=['POST'])
 def delete_movie(user_id, movie_id):
-    movie = Movie.query.get_or_404(movie_id)  #get the movie or a 404
-
+    movie = Movie.query.get_or_404(movie_id)
     db.session.delete(movie)
     db.session.commit()
-
     return redirect(url_for('get_user_movies', user_id=user_id))
-
-@app.route('/users')
-def list_users():
-    users = data_manager.get_all_users()
-    return render_template('users.html', users=users)
-
 
 if __name__ == '__main__':
     app.run(debug=True)

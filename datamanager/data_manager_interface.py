0af5fb6a4542012019badb43de42d352c8809a13
Welat-E <welaterenug@gmail.com>
2024-09-17 20:30:38 +0200

    Added some Docstrings

--- datamanager/data_manager_interface.py
+++ datamanager/data_manager_interface.py
@@ -1,30 +1,93 @@
+
+# Abstract base class defining data manager methods
 from abc import ABC, abstractmethod
 import json
 
 
-class DataManagerInterface(ABC): #declare what methods what the classes have 
+class DataManagerInterface(ABC):
+    """
+    Abstract base class to declare the required methods
+    for any data manager class that interacts with user and movie data.
+    """
 
-    @abstractmethod  #Its absrtracts functions / methods from other classes
+    @abstractmethod
     def get_all_users(self):
+        """
+        Retrieve all users.
+
+        This method must be implemented by any subclass of DataManagerInterface.
+
+        Returns:
+            list: A list of user details.
+        """
         pass
+
 
     @abstractmethod
     def get_user_movies(self, user_id):
+        """
+        Retrieve all movies associated with a specific user.
+
+        Args:
+            user_id (int): Unique identifier for the user.
+
+        Returns:
+            list: A list of movies for the given user.
+        """
         pass
 
 
+# JSON-based implementation of DataManagerInterface
 class JSONDataManager(DataManagerInterface):
+    """
+    A data manager class that manages users and movies using JSON format.
+    This class implements the methods defined in DataManagerInterface.
+    """
+
     def __init__(self):
+        """
+        Initialize the JSONDataManager with users and movies dictionaries.
+        """
         self.users = {}
         self.movies = {}
 
+
+    # Retrieves all users from the system
     def get_all_users(self):
+        """
+        Retrieve a list of all users.
+
+        Returns:
+            list: A list of all users stored in the system.
+        """
         return list(self.users.values())
 
+
+    # Retrieves all movies for a given user
     def get_user_movies(self, user_id):
+        """
+        Retrieve all movies associated with a specific user.
+
+        Args:
+            user_id (int): Unique identifier for the user.
+
+        Returns:
+            list: A list of movies for the given user, or an empty list if the user has no movies.
+        """
         return self.movies.get(user_id, [])
 
+
+    # Saves users and movies data to a JSON file
     def save_to_json(self, file_path):
+        """
+        Save the users and movies data to a JSON file.
+
+        Args:
+            file_path (str): The path to the file where data should be saved.
+
+        Returns:
+            None
+        """
         data = {
             'users': self.users,
             'movies': self.movies
@@ -32,9 +95,19 @@ class JSONDataManager(DataManagerInterface):
         with open(file_path, 'w') as f:
             json.dump(data, f)
 
+
+    # Loads users and movies data from a JSON file
     def load_from_json(self, file_path):
+        """
+        Load the users and movies data from a JSON file.
+
+        Args:
+            file_path (str): The path to the file from which data should be loaded.
+
+        Returns:
+            None
+        """
         with open(file_path, 'r') as f:
             data = json.load(f)
             self.users = data.get('users', {})
             self.movies = data.get('movies', {})
-
--- datamanager/sqlite_data_manager.py
+++ datamanager/sqlite_data_manager.py
@@ -1,3 +1,5 @@
+
+# SQLAlchemy model and data manager for SQLite database interactions
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine, select
@@ -6,13 +8,21 @@ from datamanager.data_manager_interface import DataManagerInterface
 db = SQLAlchemy()
 
 
+# Define the User model for the database
 class User(db.Model):
+    """
+    Represents a user in the database.
+    """
     __tablename__ = 'users'
     id = db.Column(db.String, primary_key=True)
     name = db.Column(db.String)
 
 
+# Define the Movie model for the database
 class Movie(db.Model):
+    """
+    Represents a movie in the database.
+    """
     __tablename__ = 'movies'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
@@ -22,34 +32,89 @@ class Movie(db.Model):
     user_id = db.Column(db.Integer)
 
 
+# SQLite-based data manager that implements DataManagerInterface
 class SQLiteDataManager(DataManagerInterface):
+    """
+    A data manager class that handles SQLite database interactions 
+    for users and movies.
+    """
+
     def __init__(self, db_file_name):
+        """
+        Initialize the SQLiteDataManager with a SQLite database file.
+
+        Args:
+            db_file_name (str): The name of the SQLite database file.
+        """
         self.engine = create_engine(f'sqlite:///{db_file_name}')
         self.Session = sessionmaker(bind=self.engine)
         db.metadata.create_all(self.engine)
 
+
+    # Retrieves all users from the database
     def get_all_users(self):
+        """
+        Retrieve all users from the database.
+
+        Returns:
+            list: A list of User objects.
+        """
         session = self.Session()
         stmt = select(User)
         result = session.execute(stmt).scalars().all()
         session.close()
         return result
 
+
+    # Retrieves all movies for a given user from the database
     def get_user_movies(self, user_id):
+        """
+        Retrieve all movies associated with a specific user.
+
+        Args:
+            user_id (int): The ID of the user.
+
+        Returns:
+            list: A list of Movie objects associated with the given user.
+        """
         session = self.Session()
         stmt = select(Movie).where(Movie.user_id == user_id)
         result = session.execute(stmt).scalars().all()
         session.close()
         return result
 
+
+    # Adds a new user to the database
     def add_user(self, user_id, name):
+        """
+        Add a new user to the database.
+
+        Args:
+            user_id (str): The unique ID of the user.
+            name (str): The name of the user.
+
+        Returns:
+            None
+        """
         session = self.Session()
         new_user = User(id=user_id, name=name)
         session.add(new_user)
         session.commit()
         session.close()
 
+
+    # Adds a new movie to the database
     def add_movie(self, movie_id, name):
+        """
+        Add a new movie to the database.
+
+        Args:
+            movie_id (int): The unique ID of the movie.
+            name (str): The name of the movie.
+
+        Returns:
+            None
+        """
         session = self.Session()
         new_movie = Movie(id=movie_id, name=name)
         session.add(new_movie)
@@ -57,8 +122,21 @@ class SQLiteDataManager(DataManagerInterface):
         session.close()
 
 
-
+    # Updates details of a movie in the database
     def update_movie(self, movie_id, name=None, director=None, year=None, rating=None):
+        """
+        Update details of an existing movie in the database.
+
+        Args:
+            movie_id (int): The ID of the movie to be updated.
+            name (str, optional): The new name of the movie.
+            director (str, optional): The new director of the movie.
+            year (int, optional): The new release year of the movie.
+            rating (float, optional): The new rating of the movie.
+
+        Returns:
+            None
+        """
         session = self.Session()
         movie = session.query(Movie).filter(Movie.id == movie_id).one_or_none()
 
@@ -78,7 +156,18 @@ class SQLiteDataManager(DataManagerInterface):
 
         session.close()
 
+
+    # Deletes a movie from the database
     def delete_movie(self, movie_id):
+        """
+        Delete a movie from the database.
+
+        Args:
+            movie_id (int): The ID of the movie to be deleted.
+
+        Returns:
+            None
+        """
         session = self.Session()
         movie = session.query(Movie).filter(Movie.id == movie_id).first()
 

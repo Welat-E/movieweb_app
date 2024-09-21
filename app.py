5f561b96b474aed174f88456a259a6bef0177350
Welat-E <welaterenug@gmail.com>
2024-09-21 12:16:55 +0200

    Creating some changes

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask import Flask, request, render_template, url_for, redirect, jsonify, a
 from datamanager.sqlite_data_manager import SQLiteDataManager, User, db, Movie
 import requests
 import os
+from flask_sqlalchemy import SQLAlchemy
 from dotenv import load_dotenv
 
 
@@ -11,8 +12,13 @@ load_dotenv()
 API_KEY = os.environ.get("API_KEY")
 
 # Configure the app with SQLite database
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///MOVIEWEB_APP/data/users_movies.sqlite"
 
+
+app.config["SQLALCHEMY_DATABASE_URI"] = (
+    "sqlite:////Users/welat/Desktop/movieweb_app/data/users_movies.sqlite"
+)
+
+X
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db.init_app(app)
 
@@ -118,41 +124,41 @@ def add_movie(user_id):
     if request.method == "POST":
         title = request.form["name"]
         params = {"apikey": API_KEY, "t": title}
-        try:
-            response = requests.get(API_URL, params=params)
-            print(response)
-            if response.status_code == 200:
-                movie_data = response.json()
-                if movie_data.get("Response") == "True":
-                    name = movie_data.get("Title")
-                    director = movie_data.get("Director")
-                    year = movie_data.get("Year")
-                    rating = movie_data.get("imdbRating")
+        # try:
+        response = requests.get(API_URL, params=params)
+        print(response)
+        if response.status_code == 200:
+            movie_data = response.json()
+            if movie_data.get("Response") == "True":
+                name = movie_data.get("Title")
+                director = movie_data.get("Director")
+                year = movie_data.get("Year")
+                rating = movie_data.get("imdbRating")
 
-                    movie = Movie(
-                        name=name,
-                        director=director,
-                        year=int(year) if year.isdigit() else None,
-                        rating=float(rating) if rating else None,
-                        user_id=user_id,
-                    )
-                    db.session.add(movie)
-                    db.session.commit()
+                movie = Movie(
+                    name=name,
+                    director=director,
+                    year=int(year) if year.isdigit() else None,
+                    rating=float(rating) if rating else None,
+                    user_id=user_id,
+                )
+                db.session.add(movie)
+                db.session.commit()
 
-                    return redirect(url_for("get_user_movies", user_id=user_id))
-                else:
-                    error_message = "Movie not found. Please try again."
-                    return render_template(
-                        "add_movie.html", user_id=user_id, error=error_message
-                    )
+                return redirect(url_for("get_user_movies", user_id=user_id))
             else:
-                error_message = "Error with the request. Please try again later."
+                error_message = "Movie not found. Please try again."
                 return render_template(
                     "add_movie.html", user_id=user_id, error=error_message
                 )
-        except Exception as e:
+        else:
+            error_message = "Error with the request. Please try again later."
+            return render_template(
+                "add_movie.html", user_id=user_id, error=error_message
+            )
+        """except Exception as e:
             print(e)
-            return render_template("500.html"), 500
+            return render_template("500.html"), 500"""
 
     return render_template("add_movie.html", user_id=user_id)
 
